c3f6f2f0ea1982b0f3bef0db062ab1c910b16bb8
Jackson Case <casejack100@protonmail.com>
2025-11-27 17:27:10 -0500

    VERSION 0.1 BABY not connected to database
    This is working preliminary code. Please help with database christian

--- src/lib.rs
+++ src/lib.rs
@@ -1,51 +1,174 @@
-pub mod lib
+pub mod lib 
 {
-enum DBRequestStatus
-{
-    SUCCESS,
-    ERROR,
-}
+    #![allow(dead_code)]
+    use serde::{Serialize, Deserialize};
 
-impl DBRequestStatus {}
-
-trait DatabaseInterface
-{
-    fn create_db()            -> DBRequestStatus;
-    fn sanitzed_sql_request() -> DBRequestStatus;
-}
-
-struct DebugDatabase
-{
-}
-
-impl DatabaseInterface for DebugDatabase 
-{
-    fn create_db() -> DBRequestStatus
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct AutoData
     {
-        DBRequestStatus::SUCCESS
+        moved: bool,
+        low:   u8,
+        outer: u8,
+        inner: u8,
     }
 
-    fn sanitzed_sql_request() -> DBRequestStatus
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct TeleopData
     {
-        DBRequestStatus::SUCCESS
-    }
-}
-
-struct Api
-{
-    db_interface: &impl DatabaseInterface,
-}
-
-impl Api 
-{
-    fn new<T: &impl DatabaseInterface>(instance: T) -> Self
-    {
-        Api{db_instance: interface}
+        low:              u8,
+        outer:            u8,
+        inner:            u8,
+        rotation_control: bool,
+        position_control: bool,
     }
 
-    fn debug_new() -> Self
+    #[derive(Serialize, Deserialize, Debug)]
+    pub enum HangState
     {
-        Api(DebugDatabase())
+        None,
+        Park,
+        Hang,
+        Level,
     }
-}
-}
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct EndgameData
+    {
+        hang:  HangState,
+        level: bool,
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct MatchData
+    {
+        match_number: u8,
+        team_number:  u16,
+        scouter_name: String,
+        auto:         AutoData,
+        teleop:       TeleopData,
+        endgame:      EndgameData,
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub enum DrivetrainTypes
+    {
+        Tank,
+        Swerve,
+        Mecanum,
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub enum MotorType
+    {
+        Falcon500,
+        KrakenX60,
+        KrakenX44,
+        NeoV1_1,
+        Neo550,
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct RobotSpec
+    {
+        drivetrain: DrivetrainTypes,
+        weight_lb:  f32,
+        motor:      MotorType,
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct RobotCapabilities
+    {
+        auto_move: bool,
+        auto_low:  bool,
+        auto_high: bool,
+        climb:     bool,
+        wheel_of_fortune: bool,
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct PitData
+    {
+        team_name:    String,
+        team_number:  u16,
+        spec:         RobotSpec,
+        capabilities: RobotCapabilities,
+        comment:      String,
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub enum DBRequestError 
+    {
+        NotImplemented,
+        ConnectionFaliure
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct ServerInfo
+    {
+        version: String,
+    }
+
+    pub trait DatabaseInterface 
+    {
+        fn get_server_info(&self) -> ServerInfo;
+
+        fn set_match_data(&self, data: MatchData) -> Result<(), DBRequestError>;
+        fn set_pit_data  (&self, data: PitData)   -> Result<(), DBRequestError>;
+
+        fn get_all_match_data(&self)                  -> Result<Vec<MatchData>, DBRequestError>;
+        fn get_all_pit_data  (&self)                  -> Result<Vec<PitData>,   DBRequestError>;
+        fn get_match_data    (&self, identifier: u32) -> Result<MatchData,      DBRequestError>;
+        fn get_pit_data      (&self, identifier: u32) -> Result<PitData,        DBRequestError>;
+    }
+
+    pub struct DebugDatabase {}
+
+    impl DatabaseInterface for DebugDatabase 
+    {
+        fn get_server_info(&self) -> ServerInfo
+        {
+            ServerInfo{version: stringify!(0.1.0).to_string()}
+        }
+
+        fn set_match_data(&self, _data: MatchData) -> Result<(), DBRequestError>
+        {
+            println!("DEBUG: Set match data"); // add identifier
+            Err(DBRequestError::NotImplemented)
+        }
+        fn set_pit_data(&self, _data: PitData)-> Result<(), DBRequestError>
+        {
+            println!("DEBUG: Set pit data"); // add identifier
+            Err(DBRequestError::NotImplemented)
+        }
+
+        fn get_all_match_data(&self) -> Result<Vec<MatchData>, DBRequestError>
+        {
+            println!("DEBUG: Get all match data");
+            Err(DBRequestError::NotImplemented)
+        }
+
+        fn get_all_pit_data(&self) -> Result<Vec<PitData>, DBRequestError>
+        {
+            println!("DEBUG: Get all pit data");
+            Err(DBRequestError::NotImplemented)
+        }
+
+        fn get_match_data(&self, identifier: u32) -> Result<MatchData, DBRequestError>
+        {
+            println!("DEBUG: Get match data with identifier: {}", identifier);
+            Err(DBRequestError::NotImplemented)
+        }
+
+        fn get_pit_data(&self, identifier: u32) -> Result<PitData, DBRequestError>
+        {
+            println!("DEBUG: Get pit data with identifier: {}", identifier);
+            Err(DBRequestError::NotImplemented)
+        }
+    } // impl DatabaseInterface for DebugDatabase
+
+    struct DatabaseReal
+    {
+
+    }
+
+} // mod lib
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,67 @@
-// use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-use actix_web::{get, post, HttpResponse, Responder};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use scoutin_frc_backend::lib::{DatabaseInterface, DebugDatabase, MatchData, PitData};
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+// Global database
+static DATABASE: DebugDatabase = DebugDatabase{};
+
+// POST
+#[post("/api/set_match_data")]
+async fn set_match_data(data: web::Json<MatchData>) -> impl Responder // UPDATE IDENTIFIER
+{
+    // Unwrap is OK because both Result types are serializable
+    HttpResponse::Ok().json(DATABASE.set_match_data(data.0).unwrap())
+}
+
+
+#[post("/api/set_pit_data")]
+async fn set_pit_data(data: web::Json<PitData>) -> impl Responder // UPDATE IDENTIFIER
+{
+    // Unwrap is OK because both Result types are serializable
+    HttpResponse::Ok().json(DATABASE.set_pit_data(data.0).unwrap())
+}
+
+// GET
+#[get("/api/get_all_match_data")]
+async fn get_all_match_data() -> impl Responder // UPDATE IDENTIFIER
+{
+    // Unwrap is OK because both Result types are serializable
+    HttpResponse::Ok().json(DATABASE.get_all_pit_data().unwrap())
+}
+
+#[get("/api/get_all_pit_data")]
+async fn get_all_pit_data() -> impl Responder // UPDATE IDENTIFIER
+{
+    // Unwrap is OK because both Result types are serializable
+    HttpResponse::Ok().json(DATABASE.get_all_pit_data().unwrap())
+}
+
+#[get("/api/get_match_data")]
+async fn get_match_data(identifier: web::Json<u32>) -> impl Responder // UPDATE IDENTIFIER
+{
+    // Unwrap is OK because both Result types are serializable
+    HttpResponse::Ok().json(DATABASE.get_match_data(identifier.to_owned()).unwrap())
+}
+
+
+#[get("/api/get_pit_data")]
+async fn get_pit_data(identifier: web::Json<u32>) -> impl Responder // UPDATE IDENTIFIER
+{
+    // Unwrap is OK because both Result types are serializable
+    HttpResponse::Ok().json(DATABASE.get_pit_data(identifier.to_owned()).unwrap())
+}
+
+#[get("/api/index")]
+async fn index() -> impl Responder 
+{
+    HttpResponse::Ok().json(DATABASE.get_server_info())
 }
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> std::io::Result<()> 
+{
     HttpServer::new(|| {
         App::new()
-            .service(hello)
+            .service(index)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
