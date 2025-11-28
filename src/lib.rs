122921743a58391074b67f13a62a02466e9055bc
Jackson Case <casejack100@protonmail.com>
2025-11-27 22:56:27 -0500

    there are mistakes in here

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,9 @@ pub mod lib
 {
     #![allow(dead_code)]
     use serde::{Serialize, Deserialize};
+    use serde_json;
+    use sqlx::mysql::MySqlPool;
+    use std::{env};
 
     #[derive(Serialize, Deserialize, Debug)]
     pub struct AutoData
@@ -105,6 +108,7 @@ pub mod lib
     #[derive(Serialize, Deserialize, Debug)]
     pub struct ServerInfo
     {
+        description: String,
         version: String,
     }
 
@@ -112,13 +116,13 @@ pub mod lib
     {
         fn get_server_info(&self) -> ServerInfo;
 
-        fn set_match_data(&self, data: MatchData) -> Result<(), DBRequestError>;
-        fn set_pit_data  (&self, data: PitData)   -> Result<(), DBRequestError>;
+        async fn set_match_data(&self, data: MatchData) -> impl Future<Output = Result<(), DBRequestError>>;
+        async fn set_pit_data  (&self, data: PitData)   -> impl Future<Output = Result<(), DBRequestError>>;
 
-        fn get_all_match_data(&self)                  -> Result<Vec<MatchData>, DBRequestError>;
-        fn get_all_pit_data  (&self)                  -> Result<Vec<PitData>,   DBRequestError>;
-        fn get_match_data    (&self, identifier: u32) -> Result<MatchData,      DBRequestError>;
-        fn get_pit_data      (&self, identifier: u32) -> Result<PitData,        DBRequestError>;
+        async fn get_all_match_data(&self)                  -> impl Future<Output = Result<Vec<MatchData>, DBRequestError>>;
+        async fn get_all_pit_data  (&self)                  -> impl Future<Output = Result<Vec<PitData>,   DBRequestError>>;
+        async fn get_match_data    (&self, identifier: u32) -> impl Future<Output = Result<MatchData,      DBRequestError>>;
+        async fn get_pit_data      (&self, identifier: u32) -> impl Future<Output = Result<PitData,        DBRequestError>>;
     }
 
     pub struct DebugDatabase {}
@@ -127,47 +131,110 @@ pub mod lib
     {
         fn get_server_info(&self) -> ServerInfo
         {
-            ServerInfo{version: stringify!(0.1.0).to_string()}
+            ServerInfo{
+                description: stringify!(This is the debug server).to_string(),
+                version: stringify!(0.1.0).to_string()}
         }
 
-        fn set_match_data(&self, _data: MatchData) -> Result<(), DBRequestError>
+        async fn set_match_data(&self, data: MatchData) -> impl Future<Output = Result<(), DBRequestError>>
         {
             println!("DEBUG: Set match data"); // add identifier
-            Err(DBRequestError::NotImplemented)
+            println!("{}", serde_json::to_string(&data).unwrap());
+            async{ Err(DBRequestError::NotImplemented)}
         }
-        fn set_pit_data(&self, _data: PitData)-> Result<(), DBRequestError>
+
+        async fn set_pit_data(&self, data: PitData) -> impl Future<Output = Result<(), DBRequestError>>
         {
             println!("DEBUG: Set pit data"); // add identifier
-            Err(DBRequestError::NotImplemented)
+            println!("{}", serde_json::to_string(&data).unwrap());
+            async{ Err(DBRequestError::NotImplemented) }
         }
 
-        fn get_all_match_data(&self) -> Result<Vec<MatchData>, DBRequestError>
+        async fn get_all_match_data(&self) -> impl Future<Output = Result<Vec<MatchData>, DBRequestError>>
         {
             println!("DEBUG: Get all match data");
-            Err(DBRequestError::NotImplemented)
+            async{ Err(DBRequestError::NotImplemented) }
         }
 
-        fn get_all_pit_data(&self) -> Result<Vec<PitData>, DBRequestError>
+        async fn get_all_pit_data(&self) -> impl Future<Output = Result<Vec<PitData>, DBRequestError>>
         {
             println!("DEBUG: Get all pit data");
-            Err(DBRequestError::NotImplemented)
+            async{ Err(DBRequestError::NotImplemented) }
         }
 
-        fn get_match_data(&self, identifier: u32) -> Result<MatchData, DBRequestError>
+        async fn get_match_data(&self, identifier: u32) -> impl Future<Output = Result<MatchData, DBRequestError>>
         {
             println!("DEBUG: Get match data with identifier: {}", identifier);
-            Err(DBRequestError::NotImplemented)
+            async{ Err(DBRequestError::NotImplemented) }
         }
 
-        fn get_pit_data(&self, identifier: u32) -> Result<PitData, DBRequestError>
+        async fn get_pit_data(&self, identifier: u32) -> impl Future<Output = Result<PitData, DBRequestError>>
         {
             println!("DEBUG: Get pit data with identifier: {}", identifier);
-            Err(DBRequestError::NotImplemented)
+            async{ Err(DBRequestError::NotImplemented) }
         }
     } // impl DatabaseInterface for DebugDatabase
 
     struct DatabaseReal
     {
+        db_connection: MySqlPool,
+    }
+
+    impl DatabaseReal
+    {
+        async fn new() -> Self
+        {
+            // If these don't work, error out and let the user try again. The compile time error should trigger immedietly after running.
+            // mysql://user:pass@host/database is the template for the database url
+            Self{db_connection: MySqlPool::connect(&env::var("DATABASE_URL").unwrap()).await.unwrap()}
+        }
+    }
+
+    impl DatabaseInterface for DatabaseReal 
+    {
+        fn get_server_info(&self) -> ServerInfo
+        {
+            ServerInfo{
+                description: stringify!(The backend using rust through actix and sqlx/mysql.).to_string(),
+                version:     stringify!(0.1.0).to_string(),
+            }
+        }
+
+        async fn set_match_data(&self, _data: MatchData) -> impl Future<Output = Result<(), DBRequestError>>
+        {
+            let result = sqlx::query("").execute(&self.db_connection).await;
+            async{ Err(DBRequestError::NotImplemented) }
+        }
+        async fn set_pit_data(&self, data: PitData) -> impl Future<Output = Result<(), DBRequestError>>
+        {
+            println!("DEBUG: Set pit data"); // add identifier
+            println!("{}", serde_json::to_string(&data).unwrap());
+            async { Err(DBRequestError::NotImplemented) }
+        }
+
+        async fn get_all_match_data(&self) -> impl Future<Output = Result<Vec<MatchData>, DBRequestError>>
+        {
+            println!("DEBUG: Get all match data");
+            async { Err(DBRequestError::NotImplemented) }
+        }
+
+        async fn get_all_pit_data(&self) -> impl Future<Output = Result<Vec<PitData>, DBRequestError>>
+        {
+            println!("DEBUG: Get all pit data");
+            async { Err(DBRequestError::NotImplemented) }
+        }
+
+        async fn get_match_data(&self, identifier: u32) -> impl Future<Output = Result<MatchData, DBRequestError>>
+        {
+            println!("DEBUG: Get match data with identifier: {}", identifier);
+            async { Err(DBRequestError::NotImplemented) }
+        }
+
+        async fn get_pit_data(&self, identifier: u32) -> impl Future<Output = Result<PitData, DBRequestError>>
+        {
+            println!("DEBUG: Get pit data with identifier: {}", identifier);
+            async { Err(DBRequestError::NotImplemented) }
+        }
 
     }
 
--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,17 @@ static DATABASE: DebugDatabase = DebugDatabase{};
 
 // POST
 #[post("/api/set_match_data")]
-async fn set_match_data(data: web::Json<MatchData>) -> impl Responder // UPDATE IDENTIFIER
+async fn set_match_data(data: web::Json<MatchData>) -> impl Responder
 {
     // Unwrap is OK because both Result types are serializable
-    HttpResponse::Ok().json(DATABASE.set_match_data(data.0).unwrap())
+    HttpResponse::Ok().json(DATABASE.set_match_data(data.into_inner()).await.await.unwrap())
 }
 
-
 #[post("/api/set_pit_data")]
-async fn set_pit_data(data: web::Json<PitData>) -> impl Responder // UPDATE IDENTIFIER
+async fn set_pit_data(data: web::Json<PitData>) -> impl Responder
 {
     // Unwrap is OK because both Result types are serializable
-    HttpResponse::Ok().json(DATABASE.set_pit_data(data.0).unwrap())
+    HttpResponse::Ok().json(DATABASE.set_pit_data(data.into_inner()).await.await.unwrap())
 }
 
 // GET
@@ -25,21 +24,21 @@ async fn set_pit_data(data: web::Json<PitData>) -> impl Responder // UPDATE IDEN
 async fn get_all_match_data() -> impl Responder // UPDATE IDENTIFIER
 {
     // Unwrap is OK because both Result types are serializable
-    HttpResponse::Ok().json(DATABASE.get_all_pit_data().unwrap())
+    HttpResponse::Ok().json(DATABASE.get_all_pit_data().await.await.unwrap())
 }
 
 #[get("/api/get_all_pit_data")]
 async fn get_all_pit_data() -> impl Responder // UPDATE IDENTIFIER
 {
     // Unwrap is OK because both Result types are serializable
-    HttpResponse::Ok().json(DATABASE.get_all_pit_data().unwrap())
+    HttpResponse::Ok().json(DATABASE.get_all_pit_data().await.await.unwrap())
 }
 
 #[get("/api/get_match_data")]
 async fn get_match_data(identifier: web::Json<u32>) -> impl Responder // UPDATE IDENTIFIER
 {
     // Unwrap is OK because both Result types are serializable
-    HttpResponse::Ok().json(DATABASE.get_match_data(identifier.to_owned()).unwrap())
+    HttpResponse::Ok().json(DATABASE.get_match_data(identifier.to_owned()).await.await.unwrap())
 }
 
 
@@ -47,7 +46,7 @@ async fn get_match_data(identifier: web::Json<u32>) -> impl Responder // UPDATE 
 async fn get_pit_data(identifier: web::Json<u32>) -> impl Responder // UPDATE IDENTIFIER
 {
     // Unwrap is OK because both Result types are serializable
-    HttpResponse::Ok().json(DATABASE.get_pit_data(identifier.to_owned()).unwrap())
+    HttpResponse::Ok().json(DATABASE.get_pit_data(identifier.to_owned()).await.await.unwrap())
 }
 
 #[get("/api/index")]
